0e062019e1b3d17e57e7f0f20e814b7e50f68924
Marlon Guerra <mguerramulford@gmail.com>
2017-05-06 15:34:19 -0500

    Interfaz y String terminados 2

--- app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
+++ app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
@@ -2,6 +2,7 @@ package com.example.android.zxycalzadodeportivo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -43,9 +44,53 @@ public class Principal extends AppCompatActivity {
         marcaCombo = (Spinner)findViewById(R.id.cmdmarca);
         marcaOpciones = this.getResources().getStringArray(R.array.marca);
         marcaAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, marcaOpciones);
+        marcaCombo.setAdapter(marcaAdapter);
 
         cantidad = (EditText)findViewById(R.id.txtcantidad);
         cotizacion = (EditText)findViewById(R.id.txtcotizacion);
+    }
 
+    public void Calcular(View v)
+    {
+        int op;
+        double n1, n2, res = 0;
+        if(validar())
+        {
+            n1 = Double.parseDouble(cajanumero1.getText().toString());
+            n2 = Double.parseDouble(cajanumero2.getText().toString());
+            op = comboOpciones.getSelectedItemPosition();
+            switch (op){
+                case 0:
+                    res = n1 + n2;
+                    break;
+                case 1:
+                    res = n1 - n2;
+                    break;
+                case 2:
+                    res = n1 * n2;
+                    break;
+                case 3:
+                    res = n1 / n2;
+                    break;
+            }
+            cotizacion.setText(""+precio);
+            cantidad.requestFocus();
+        }
+
+    }
+
+    public boolean validar()
+    {
+        if(cantidad.getText().toString().isEmpty())
+        {
+            cantidad.setError(this.getResources().getString(R.string.error_vacio));
+            cantidad.requestFocus();
+            return false;
+        }else if(cantidad.getText().toString() <= 0)
+                {
+                    cantidad.setError(this.getResources().getString(R.string.error_menor_cero));
+                    cantidad.requestFocus();
+                }
+        return true;
     }
 }
