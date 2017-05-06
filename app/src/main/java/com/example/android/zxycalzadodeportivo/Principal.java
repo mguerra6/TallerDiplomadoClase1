1be41941079293be40aa31c983a611284213e9bb
Marlon Guerra <mguerramulford@gmail.com>
2017-05-06 15:15:31 -0500

    Interfaz y String terminados 2

--- app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
+++ app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
@@ -22,11 +22,30 @@ public class Principal extends AppCompatActivity {
     private ArrayAdapter marcaAdapter;
 
     private EditText cantidad;
+    private EditText cotizacion;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        sexoCombo = (Spinner)findViewById(R.id.cmdsexo);
+        sexoOpciones = this.getResources().getStringArray(R.array.sexo);
+        sexoAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sexoOpciones);
+        sexoCombo.setAdapter(sexoAdapter);
+
+        tipoCombo = (Spinner)findViewById(R.id.cmdtipo);
+        tipoOpciones = this.getResources().getStringArray(R.array.tipo);
+        tipoAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tipoOpciones);
+        tipoCombo.setAdapter(tipoAdapter);
+
+        marcaCombo = (Spinner)findViewById(R.id.cmdmarca);
+        marcaOpciones = this.getResources().getStringArray(R.array.marca);
+        marcaAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, marcaOpciones);
+
+        cantidad = (EditText)findViewById(R.id.txtcantidad);
+        cotizacion = (EditText)findViewById(R.id.txtcotizacion);
+
     }
 }
