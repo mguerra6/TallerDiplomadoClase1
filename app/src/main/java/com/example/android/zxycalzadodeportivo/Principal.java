db36367f16eb5055176530ef94e71a66792198b5
Marlon Guerra <mguerramulford@gmail.com>
2017-05-06 14:54:00 -0500

    Interfaz y String terminados

--- app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
+++ app/src/main/java/com/example/android/zxycalzadodeportivo/Principal.java
@@ -2,8 +2,27 @@ package com.example.android.zxycalzadodeportivo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class Principal extends AppCompatActivity {
+    private Spinner sexoCombo;
+    private String [] sexoOpciones;
+    private ArrayAdapter sexoAdapter;
+
+    private Spinner tipoCombo;
+    private String[] tipoOpciones;
+    private ArrayAdapter tipoAdapter;
+
+    private Spinner marcaCombo;
+    private String[] marcaOpciones;
+    private ArrayAdapter marcaAdapter;
+
+    private EditText cantidad;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
